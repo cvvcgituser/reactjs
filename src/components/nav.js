996e06b08b0beab14aa6659accb98216000ef226
cvvcgituser <115985101+cvvcgituser@users.noreply.github.com>
2022-12-27 16:04:26 +0530

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,9 +8,6 @@ export default class Nav extends Component {
           <div className="nav-wrapper">
             <a href="/" className="brand-logo">Logo</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><a href="/">HOME</a></li>
-              <li><a href="https://www.facebook.com">FACEBOOK</a></li>
-              <li><a href="https://www.linkedin.com">LINKED IN</a></li>
             </ul>
           </div>
         </nav>
