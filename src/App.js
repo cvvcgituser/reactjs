81e4c44b619385e7ccc37c214b3742835d97e1b2
cvvcgituser <115985101+cvvcgituser@users.noreply.github.com>
2022-12-27 16:26:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Nav from './components/nav';
-import Footer from './components/footer';
 import MainContainer from './components/main-container';
 
 
