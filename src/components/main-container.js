d19600eac8fdcb88693a74f63847e998172a497e
cvvcgituser <115985101+cvvcgituser@users.noreply.github.com>
2022-12-27 16:05:51 +0530

    Update main-container.js

--- src/components/main-container.js
+++ src/components/main-container.js
@@ -65,7 +65,7 @@ export default class MainContainer extends Component {
         <div className="row">
           <div className="col s12">
             <div className="section">
-              <h3 className="header">Cryptocurrency Monitor</h3>
+              <h3 className="header">Hi ACS !,ReactJs App for test deployment in Amazon ECS</h3>
             </div>
           </div>
         </div>
