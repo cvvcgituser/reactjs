cfbdb9907a7f0d64f4d04a193b545681b85ede63
cvvcgituser <115985101+cvvcgituser@users.noreply.github.com>
2022-12-27 16:12:39 +0530

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,13 +4,6 @@ import React, { Component } from 'react';
 export default class Nav extends Component {
   render() {
     return (
-      <nav>
-          <div className="nav-wrapper">
-            <a href="/" className="brand-logo">Logo</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-            </ul>
-          </div>
-        </nav>
     );
   }
 }
